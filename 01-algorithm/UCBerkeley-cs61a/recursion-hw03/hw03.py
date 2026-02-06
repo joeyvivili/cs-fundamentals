32404b1816138feaaed9edfa400a27826f611c44
Vivi Li <141885322+joeyvivili@users.noreply.github.com>
2026-02-06 10:30:04 +0800

    Add files via upload

---
+++ 01-algorithm/UCBerkeley-cs61a/recursion-hw03/hw03.py
@@ -0,0 +1,245 @@
+LAB_SOURCE_FILE=__file__
+
+
+HW_SOURCE_FILE=__file__
+
+
+def num_eights(n):
+    """Returns the number of times 8 appears as a digit of n.
+
+    >>> num_eights(3)
+    0
+    >>> num_eights(8)
+    1
+    >>> num_eights(88888888)
+    8
+    >>> num_eights(2638)
+    1
+    >>> num_eights(86380)
+    2
+    >>> num_eights(12345)
+    0
+    >>> num_eights(8782089)
+    3
+    >>> from construct_check import check
+    >>> # ban all assignment statements
+    >>> check(HW_SOURCE_FILE, 'num_eights',
+    ...       ['Assign', 'AnnAssign', 'AugAssign', 'NamedExpr', 'For', 'While'])
+    True
+    """
+    "*** YOUR CODE HERE ***"
+    if n <= 0:
+        return 0
+    elif n % 10 == 8:
+        return 1 + num_eights(n // 10)
+    else:
+        return num_eights(n // 10)
+    
+
+
+def digit_distance(n):
+    """Determines the digit distance of n.
+
+    >>> digit_distance(3)
+    0
+    >>> digit_distance(777)
+    0
+    >>> digit_distance(314)
+    5
+    >>> digit_distance(31415926535)
+    32
+    >>> digit_distance(3464660003)
+    16
+    >>> from construct_check import check
+    >>> # ban all loops
+    >>> check(HW_SOURCE_FILE, 'digit_distance',
+    ...       ['For', 'While'])
+    True
+    """
+    "*** YOUR CODE HERE ***"
+    if n < 10:
+        return 0
+    else:
+        curr, n = n % 10, n // 10
+        return abs(curr - n % 10) + digit_distance(n)
+    
+
+def interleaved_sum(n, odd_func, even_func):
+    """Compute the sum odd_func(1) + even_func(2) + odd_func(3) + ..., up
+    to n.
+
+    >>> identity = lambda x: x
+    >>> square = lambda x: x * x
+    >>> triple = lambda x: x * 3
+    >>> interleaved_sum(5, identity, square) # 1   + 2*2 + 3   + 4*4 + 5
+    29
+    >>> interleaved_sum(5, square, identity) # 1*1 + 2   + 3*3 + 4   + 5*5
+    41
+    >>> interleaved_sum(4, triple, square)   # 1*3 + 2*2 + 3*3 + 4*4
+    32
+    >>> interleaved_sum(4, square, triple)   # 1*1 + 2*3 + 3*3 + 4*3
+    28
+    >>> from construct_check import check
+    >>> check(HW_SOURCE_FILE, 'interleaved_sum', ['While', 'For', 'Mod']) # ban loops and %
+    True
+    """
+    "*** YOUR CODE HERE ***"
+    def f1(i):
+        if i == n:
+            return odd_func(n)
+        else:
+            return odd_func(i) + f2(i + 1)
+    def f2(j):
+        if j == n:
+            return even_func(j)
+        else:
+            return even_func(j) + f1(j + 1)
+    return f1(1)
+
+
+def next_larger_coin(coin):
+    """Returns the next larger coin in order.
+    >>> next_larger_coin(1)
+    5
+    >>> next_larger_coin(5)
+    10
+    >>> next_larger_coin(10)
+    25
+    >>> next_larger_coin(2) # Other values return None
+    """
+    if coin == 1:
+        return 5
+    elif coin == 5:
+        return 10
+    elif coin == 10:
+        return 25
+
+def next_smaller_coin(coin):
+    """Returns the next smaller coin in order.
+    >>> next_smaller_coin(25)
+    10
+    >>> next_smaller_coin(10)
+    5
+    >>> next_smaller_coin(5)
+    1
+    >>> next_smaller_coin(2) # Other values return None
+    """
+    if coin == 25:
+        return 10
+    elif coin == 10:
+        return 5
+    elif coin == 5:
+        return 1
+
+def count_coins(total):
+    """Return the number of ways to make change using coins of value of 1, 5, 10, 25.
+    >>> count_coins(15)
+    6
+    >>> count_coins(10)
+    4
+    >>> count_coins(20)
+    9
+    >>> count_coins(100) # How many ways to make change for a dollar?
+    242
+    >>> count_coins(200)
+    1463
+    >>> from construct_check import check
+    >>> # ban iteration
+    >>> check(HW_SOURCE_FILE, 'count_coins', ['While', 'For'])
+    True
+    """
+    "*** YOUR CODE HERE ***"
+    def f(total, coin):
+        if total == 0:
+            return 1
+        if total < 0 or coin is None:
+            return 0
+
+        # Option 1: use current coin
+        with_coin = f(total - coin, coin)
+
+        # Option 2: skip current coin
+        without_coin = f(total, next_smaller_coin(coin))
+
+        return with_coin + without_coin
+    return f(total, 25)
+
+
+
+def print_move(origin, destination):
+    """Print instructions to move a disk."""
+    print("Move the top disk from rod", origin, "to rod", destination)
+
+def move_stack(n, start, end):
+    """Print the moves required to move n disks on the start pole to the end
+    pole without violating the rules of Towers of Hanoi.
+
+    n -- number of disks
+    start -- a pole position, either 1, 2, or 3
+    end -- a pole position, either 1, 2, or 3
+
+    There are exactly three poles, and start and end must be different. Assume
+    that the start pole has at least n disks of increasing size, and the end
+    pole is either empty or has a top disk larger than the top n start disks.
+
+    >>> move_stack(1, 1, 3)
+    Move the top disk from rod 1 to rod 3
+    >>> move_stack(2, 1, 3)
+    Move the top disk from rod 1 to rod 2
+    Move the top disk from rod 1 to rod 3
+    Move the top disk from rod 2 to rod 3
+    >>> move_stack(3, 1, 3)
+    Move the top disk from rod 1 to rod 3
+    Move the top disk from rod 1 to rod 2
+    Move the top disk from rod 3 to rod 2
+    Move the top disk from rod 1 to rod 3
+    Move the top disk from rod 2 to rod 1
+    Move the top disk from rod 2 to rod 3
+    Move the top disk from rod 1 to rod 3
+    """
+    assert 1 <= start <= 3 and 1 <= end <= 3 and start != end, "Bad start/end"
+    "*** YOUR CODE HERE ***"
+    """ Logistic: 
+        To move n disks from start to end:
+            1. Move n−1 disks from start to the auxiliary rod
+            2. Move the largest disk from start to end
+            3. Move n−1 disks from the auxiliary rod to end
+        "To move n disks: move n−1 out of the way, move the biggest, then move n−1 back."
+    """
+    if n == 1:
+        print_move(start, end)
+    else:
+        aux = 6 - start - end   ## since there are 3 rods (1,2,3) in total, then start+aux+end = 1+2+3 = 6 
+        move_stack(n - 1, start, aux)
+        print_move(start, end)
+        move_stack(n - 1, aux, end)
+    """If keep track of the number of moves it takes to move n:
+    if n == 1:
+        print_move(start, end)
+        return 1
+    else:
+        aux = 6 - start - end
+        count = 0
+        count += move_stack(n - 1, start, aux)
+        print_move(start, end)
+        count += 1
+        count += move_stack(n - 1, aux, end)
+        return count
+    """
+
+
+from operator import sub, mul
+
+def make_anonymous_factorial():
+    """Return the value of an expression that computes factorial.
+
+    >>> make_anonymous_factorial()(5)
+    120
+    >>> from construct_check import check
+    >>> # ban any assignments or recursion
+    >>> check(HW_SOURCE_FILE, 'make_anonymous_factorial',
+    ...     ['Assign', 'AnnAssign', 'AugAssign', 'NamedExpr', 'FunctionDef', 'Recursion'])
+    True
+    """
+    return 'YOUR_EXPRESSION_HERE'
+
